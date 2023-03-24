c7a2ea8c4c8620888301fb6c8f44fc294b6b3dcc
soutetsu <soutetsu@gmail.com>
2023-03-24 19:08:08 +0900

    add comment

--- app/controllers/shopify/webhooks_controller.rb
+++ app/controllers/shopify/webhooks_controller.rb
@@ -1,5 +1,7 @@
 class Shopify::WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
+  # webhook を厳密に検証する場合はコメントアウト
+  # before_action :verify_webhook
 
   def order_update
     Rails.logger.debug JSON.pretty_generate(params)
@@ -12,4 +14,19 @@ class Shopify::WebhooksController < ApplicationController
       head :bad_request
     end
   end
+
+  private
+
+  # webhook を厳密に検証する場合はコメントアウト
+  # def verify_webhook
+  #   request.body.rewind
+  #   data = request.body.read
+  #   secret = Rails.application.credentials.shopify[:webhook][:shared_secret]
+  #   requested_hmac = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
+  #   calculated_hmac = Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', secret, data))
+  #   unless ActiveSupport::SecurityUtils.secure_compare(calculated_hmac, requested_hmac)
+  #     Rails.logger.error '[shopify:webhooks] Invalid request hmac!'
+  #     head :bad_request
+  #   end
+  # end
 end
